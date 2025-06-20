dbd4ba7f77ee35e249e2f8cdd683cc0962a0305a
thbueno <thiagobueno.s@gmail.com>
2025-06-20 23:58:56 +0700

    First Capsule

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">First Capsule!</ThemedText>
+        <ThemedText type="title">First Capsule app!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
